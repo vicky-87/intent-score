1ac5e5aaa23b9c4b87e506d42e1494f84bff3c01
vicky-87 <ahmadrofikil46@gmail.com>
2020-02-25 14:39:48 +0700

    Latihan 1 Modul 5 (belum slesai)

--- app/src/main/java/id/putraprima/skorbola/MainActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MainActivity.java
@@ -1,21 +1,49 @@
 package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ImageViewCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String HOMETEAM_KEY = "hometeam";
+    public static final String AWAYTEAM_KEY = "awayteam";
+    private EditText hometeamInput;
+    private EditText awayteamInput;
+    private ImageView homeLogo;
+    private ImageView awayLogo;
+
+    private static final String TAG = MainActivity.class.getCanonicalName();
+    private static final int GALLERY_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TODO
-        //Fitur Main Activity
-        //1. Validasi Input Home Team
-        //2. Validasi Input Away Team
-        //3. Ganti Logo Home Team
-        //4. Ganti Logo Away Team
-        //5. Next Button Pindah Ke MatchActivity
+        hometeamInput = findViewById(R.id.home_team);
+        awayteamInput = findViewById(R.id.away_team);
+    }
+
+    public void handleNext(View view) {
+        String homeTeam = hometeamInput.getText().toString();
+        String awayTeam = awayteamInput.getText().toString();
+
+
+        Intent intent =  new Intent(this, MatchActivity.class);
+        intent.putExtra(HOMETEAM_KEY, homeTeam);
+        intent.putExtra(AWAYTEAM_KEY, awayTeam);
+        startActivity(intent);
     }
 }
+//TODO
+//Fitur Main Activity
+//1. Validasi Input Home Team
+//2. Validasi Input Away Team
+//3. Ganti Logo Home Team
+//4. Ganti Logo Away Team
+//5. Next Button Pindah Ke MatchActivity
--- app/src/main/java/id/putraprima/skorbola/MatchActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MatchActivity.java
@@ -2,17 +2,49 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static id.putraprima.skorbola.MainActivity.AWAYTEAM_KEY;
+import static id.putraprima.skorbola.MainActivity.HOMETEAM_KEY;
 
 public class MatchActivity extends AppCompatActivity {
+    private TextView hometeamText;
+    private TextView awayteamText;
+    private TextView homeCount;
+    private TextView awayCount;
+    private TextView resultCount;
+
+    int counterHome = 0;
+    int counterAway = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+
         //TODO
-        //1.Menampilkan detail match sesuai data dari main activity
-        //2.Tombol add score menambahkan satu angka dari angka 0, setiap kali di tekan
-        //3.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang ke ResultActivity, jika seri di kirim text "Draw"
+        hometeamText = findViewById(R.id.home_team);
+        awayteamText = findViewById(R.id.away_team);
+        //COUNT
+        homeCount = (TextView) findViewById(R.id.score_home);
+        awayCount = (TextView) findViewById(R.id.score_away);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            // TODO: display value here
+            hometeamText.setText(extras.getString(HOMETEAM_KEY));
+            awayteamText.setText(extras.getString(AWAYTEAM_KEY));
+        }
+    }
+
+    public void handleCek(View view) {
+        Intent intent =  new Intent(this, ResultActivity.class);
+        startActivity(intent);
     }
 }
+//1.Menampilkan detail match sesuai data dari main activity
+//2.Tombol add score menambahkan satu angka dari angka 0, setiap kali di tekan
+//3.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang ke ResultActivity, jika seri di kirim text "Draw"
